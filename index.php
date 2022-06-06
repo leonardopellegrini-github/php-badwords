7f79f46bd4d2c1f1c7ca8f58ba360e895632dce1
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-06-06 20:26:07 +0200

    stampo la variabile testo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-  
+  $testo = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+  $lunghezzaTesto = strlen("$testo");
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +12,8 @@
   <title>PHP BAD WORDS</title>
 </head>
 <body>
-  <h1>PROVA</h1>
+  <h1>Questo è il testo</h1>
+  <p><?php echo $testo ?></p>
 
   
 </body>
